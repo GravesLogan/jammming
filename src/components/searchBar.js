5e096175950736de0ddf16ce48a3217b496c0dfd
Logan Graves <graveslogan@comcast.net>
2025-08-17 14:53:40 -0400

    Set up some of the initial environment and set up a hello world.

---
+++ src/components/searchBar.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import styles from '../styles/searchBar.module.css';
+
+
+export default function searchBar(props) {
+
+    return (
+        <h1>Hello World!</h1>
+    )
+}
