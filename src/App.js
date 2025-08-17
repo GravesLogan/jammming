e56c224a54e00dc24076e357bf548f86a7f4d592
Logan Graves <graveslogan@comcast.net>
2025-08-17 16:44:46 -0400

    Added some basic reading from a form for searching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
+import React from 'react';
 
+// Importing components
+import Playlist from './components/Playlist';
 import SearchBar from './components/SearchBar';
+import SearchResults from './components/SearchResults';
+import Track from './components/Track';
+import Tracklist from './components/Tracklist';
 
 function App() {
   return (
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,28 @@
-import React, { useState } from 'react';
+import React, { use, useState } from 'react';
 import styles from '../styles/SearchBar.module.css';
 
 
-export default function SearchBar(props) {
+export default function SearchBar() {
+
+    const [search, setSearch] = useState('');
+
+    function handleOnChange(e) {
+        setSearch(e.target.value);
+    }
+
+    function handleOnSubmit(e) {
+        e.preventDefault();
+        console.log('searching for:', search);
+    }
+    
 
     return (
-        <h1>Hello World!</h1>
+        <div>
+            <form onSubmit={handleOnSubmit}>
+                <input id='search' name='search' placeholder='search by title, name, genre, etc...' onChange={handleOnChange}></input>
+                <button type='submit'>Search</button>
+            </form>
+            <h1>Searching for {search}</h1>
+        </div>
     )
 }
